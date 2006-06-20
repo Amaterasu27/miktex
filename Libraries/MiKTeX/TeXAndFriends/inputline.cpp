27dfd3e61f8369b40b7097da021542d726165815
csc <csc@mycompany.com>
2006-06-20 20:26:02 +0000

    Use new methods to open streams on compressed input files.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@179 3fc76331-e2db-4b81-af9c-3600f57961af

--- Libraries/MiKTeX/TeXAndFriends/inputline.cpp
+++ Libraries/MiKTeX/TeXAndFriends/inputline.cpp
@@ -61,185 +61,6 @@ WebAppInputLine::Finalize ()
 
 /* _________________________________________________________________________
 
-   AutoGZ
-   _________________________________________________________________________ */
-
-class gzclose_
-{
-public:
-  void
-  operator() (/*[in]*/ gzFile gz)
-  {
-    gzclose (gz);
-  }
-};
-
-typedef AutoResource<gzFile, gzclose_> AutoGZ;
-
-/* _________________________________________________________________________
-
-   AutoBZ2
-   _________________________________________________________________________ */
-
-class BZ2_bzclose_
-{
-public:
-  void
-  operator() (/*[in]*/ BZFILE * bz2)
-  {
-    BZ2_bzclose (bz2);
-  }
-};
-
-typedef AutoResource<BZFILE *, BZ2_bzclose_> AutoBZ2;
-
-/* _________________________________________________________________________
-
-   GzReaderThread
-   _________________________________________________________________________ */
-
-struct CReaderThreadArg
-{
-  gzFile	gzin;
-  BZFILE *	bzin;
-  FILE *	fileout;
-};
-
-STATICFUNC(unsigned)
-__stdcall
-GzReaderThread (/*[in]*/ void * pv)
-{
-  try
-    {
-      auto_ptr<CReaderThreadArg>
-	prta (reinterpret_cast<CReaderThreadArg*>(pv));
-      AutoFILE autoClose (prta->fileout);
-      assert (prta->gzin != 0 || prta->bzin != 0);
-      assert (prta->gzin == 0 || prta->bzin == 0);
-      assert (prta->fileout != 0);
-      char buf[4096];
-      int len;
-      if (prta->gzin != 0)
-	{
-	  AutoGZ autoClose2 (prta->gzin);
-	  while (! ferror(prta->fileout)
-		 && (len = gzread(prta->gzin, buf, ARRAY_SIZE(buf))) > 0)
-	    {
-	      fwrite (buf, 1, len, prta->fileout);
-	    }
-	  if (len < 0)
-	    {
-	      FATAL_MIKTEX_ERROR (T_("GzReaderThread"),
-				  T_("gzread() failed for some reason."),
-				  0);
-	    }
-	}
-      else
-	{
-	  AutoBZ2 autoClose3 (prta->bzin);
-	  while (! ferror(prta->fileout)
-		 && (len = BZ2_bzread(prta->bzin, buf, ARRAY_SIZE(buf))) > 0)
-	    {
-	      fwrite (buf, 1, len, prta->fileout);
-	    }
-	  int bzerr;
-	  BZ2_bzerror (prta->bzin, &bzerr);
-	  if (bzerr != BZ_OK)
-	    {
-	      FATAL_MIKTEX_ERROR (T_("GzReaderThread"),
-				  T_("BZ2_bzread() failed for some reason."),
-				  0);
-	    }
-	}
-      if (ferror(prta->fileout))
-	{
-	  FATAL_CRT_ERROR (T_("fwrite"), 0);
-	}
-    }
-  catch (const exception &)
-    {
-    }
-  return (0);
-}
-
-/* _________________________________________________________________________
-   
-   OpenGzInputStream
-   _________________________________________________________________________ */
-
-STATICFUNC(FILE *)
-OpenGzInputStream (/*[in]*/ const PathName & path)
-{
-  gzFile gzin = 0;
-  BZFILE * bzin = 0;
-  AutoGZ autoGz;
-  AutoBZ2 autoBz2;
-  if (path.HasExtension(T_(".gz")))
-    {
-      gzin = gzopen(path.Get(), T_("rb"));
-      if (gzin == 0)
-	{
-	  FATAL_MIKTEX_ERROR (T_("OpenGzInputStream"),
-			      T_("gzopen() failed for some."),
-			      path.Get());
-	}
-      autoGz.Attach (gzin);
-    }
-  else
-    {
-      bzin = BZ2_bzopen(path.Get(), T_("rb"));
-      if (bzin == 0)
-	{
-	  FATAL_MIKTEX_ERROR (T_("OpenGzInputStream"),
-			      T_("BZ2_bzopen() failed for some."),
-			      path.Get());
-	}
-      autoBz2.Attach (bzin);
-    }
-  int handles[2];
-  if (_pipe(handles, 4096, _O_TEXT) != 0)
-    {
-      FATAL_CRT_ERROR (T_("_pipe"), 0);
-    }
-  FILE * pfileIn = _fdopen(handles[0], T_("rb"));
-  if (pfileIn == 0)
-    {
-      int err = errno;
-      _close (handles[0]);
-      _close (handles[1]);
-      FATAL_CRT_ERROR_2 (T_("_fdopen"), err, 0);
-    }
-  FILE * pfileOut = _fdopen(handles[1], T_("wb"));
-  if (pfileOut == 0)
-    {
-      int err = errno;
-      fclose (pfileIn);
-      _close (handles[1]);
-      FATAL_CRT_ERROR_2 (T_("_fdopen"), err, 0);
-    }
-  CReaderThreadArg * pthreadarg = new CReaderThreadArg;
-  pthreadarg->gzin = gzin;
-  pthreadarg->bzin = bzin;
-  pthreadarg->fileout = pfileOut;
-  unsigned threadID;
-  HANDLE hThread =
-    reinterpret_cast<HANDLE>
-    (_beginthreadex(0, 0, GzReaderThread, pthreadarg, 0, &threadID));
-  if (hThread == 0)
-    {
-      int err = errno;
-      fclose (pfileIn);
-      fclose (pfileOut);
-      FATAL_CRT_ERROR_2 (T_("_beginthreadex"), err, 0);
-    }
-  CloseHandle (hThread);
-  autoGz.Detach ();
-  autoBz2.Detach ();
-  return (pfileIn);
-}
-
-/* _________________________________________________________________________
-
    WebAppInputLine::MangleNameOfFile
    _________________________________________________________________________ */
 
@@ -377,30 +198,45 @@ WebAppInputLine::OpenInputFile (/*[in]*/ C4P::C4P_text &	f,
       return (false);
     }
 
-  bool isCompressedFile =
-    (fqNameOfFile.HasExtension(T_(".gz"))
-     || fqNameOfFile.HasExtension(T_(".bz2")));
-  
-  FILE * pfile;
-  
-  if (isCompressedFile)
+  FILE * pfile = 0;
+
+  try
     {
-      pfile = OpenGzInputStream(fqNameOfFile);
-    }
-  else
-    {
+      if (fqNameOfFile.HasExtension(T_(".gz")))
+	{
+	  pfile = pSession->OpenGZipFile(fqNameOfFile);
+	}
+      else if (fqNameOfFile.HasExtension(T_(".bz2")))
+	{
+	  pfile = pSession->OpenBZip2File(fqNameOfFile);
+	}
+      else
+	{
 #if 0
-      FileShare share = FileShare::Read;
+	  FileShare share = FileShare::Read;
 #else
-      FileShare share = FileShare::ReadWrite;
+	  FileShare share = FileShare::ReadWrite;
 #endif
-      pfile =
-	pSession->TryOpenFile(fqNameOfFile.Get(),
-			      FileMode::Open,
-			      FileAccess::Read,
-			      false,
-			      share);
+	  pfile =
+	    pSession->OpenFile(fqNameOfFile.Get(),
+			       FileMode::Open,
+			       FileAccess::Read,
+			       false,
+			       share);
+	}
     }
+#if defined(MIKTEX_WINDOWS)
+  catch (const SharingViolationException &)
+    {
+    }
+#endif
+  catch (const UnauthorizedAccessException &)
+    {
+    }
+  catch (const FileNotFoundException &)
+    {
+    }
+
 
   if (pfile == 0)
     {
