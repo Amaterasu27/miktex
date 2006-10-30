eef97d738c4c1c2a0197cc214324de24d78c43cd
csc <csc@mycompany.com>
2006-10-30 19:28:28 +0000

    Run initexmf, even if we prepare to use a live MiKTeX system.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.5@510 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/MiKTeX/Wizards/Setup/MFC/Setup-version.h
+++ Programs/MiKTeX/Wizards/Setup/MFC/Setup-version.h
@@ -17,5 +17,5 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA. */
 
-#define VER_FILEVERSION 2,5,2471,0
-#define VER_FILEVERSION_STR "2.5.2471"
+#define VER_FILEVERSION 2,5,2494,0
+#define VER_FILEVERSION_STR "2.5.2494"
