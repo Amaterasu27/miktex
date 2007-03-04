af448e4861a60e20f8ab7f4a819afa429cf9f19c
csc <csc@mycompany.com>
2007-03-04 20:33:06 +0000

    New default installation directory for restricted user setup.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@632 3fc76331-e2db-4b81-af9c-3600f57961af

--- Libraries/MiKTeX/Core/win/win.cpp
+++ Libraries/MiKTeX/Core/win/win.cpp
@@ -123,8 +123,17 @@ SessionImpl::DefaultConfig (/*[in]*/ bool sharedSetup)
 {
   StartupConfig ret;
   tstring product = (IsMiKTeXDirect() ? T_("MiKTeXDirect") : T_("MiKTeX"));
-  ret.installRoot = MyGetFolderPath(CSIDL_PROGRAM_FILES, true);
-  ret.installRoot += T_("MiKTeX") T_(" ") T_(MIKTEX_SERIES_STR);
+  if (RunningAsAdministrator() || RunningAsPowerUser())
+    {
+      ret.installRoot = MyGetFolderPath(CSIDL_PROGRAM_FILES, true);
+      ret.installRoot += T_("MiKTeX") T_(" ") T_(MIKTEX_SERIES_STR);
+    }
+  else
+    {
+      ret.installRoot =
+	Utils::GetFolderPath(CSIDL_LOCAL_APPDATA, CSIDL_APPDATA, true);
+      ret.installRoot += T_("MiKTeX") T_(" ") T_(MIKTEX_SERIES_STR);
+    }
   if (sharedSetup)
     {
       ret.commonDataRoot = MyGetFolderPath(CSIDL_COMMON_APPDATA, true);
