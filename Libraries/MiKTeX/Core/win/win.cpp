425a5e1ed3683132169183887143a61b110d399a
csc <csc@mycompany.com>
2007-04-27 19:08:29 +0000

    Per-user installation: use correct installation directory.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@761 3fc76331-e2db-4b81-af9c-3600f57961af

--- Libraries/MiKTeX/Core/win/win.cpp
+++ Libraries/MiKTeX/Core/win/win.cpp
@@ -124,9 +124,9 @@ SessionImpl::DefaultConfig (/*[in]*/ bool sharedSetup)
   StartupConfig ret;
   tstring product = (IsMiKTeXDirect() ? T_("MiKTeXDirect") : T_("MiKTeX"));
   if (sharedSetup
-      && (! IsWindowsNT()
-	  || IsUserAnAdministrator()
-	  || IsUserAPowerUser()))
+      || ! IsWindowsNT()
+      || IsUserAnAdministrator()
+      || IsUserAPowerUser())
     {
       ret.installRoot = MyGetFolderPath(CSIDL_PROGRAM_FILES, true);
       ret.installRoot += T_("MiKTeX") T_(" ") T_(MIKTEX_SERIES_STR);
