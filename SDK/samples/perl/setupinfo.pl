1dd4a0abf1b9a7067e58c71608c4adc24805c2a1
csc <csc@mycompany.com>
2007-05-14 18:52:52 +0000

    New SDK sample: packageinfo.pl.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.6@771 3fc76331-e2db-4b81-af9c-3600f57961af

--- SDK/samples/perl/setupinfo.pl
+++ SDK/samples/perl/setupinfo.pl
@@ -1,8 +1,6 @@
 use Win32::OLE;
-use Win32::OLE::Variant;
 my $session = Win32::OLE->new('MiKTeX.Session')
   or die "MiKTeX is not properly installed.\n";
-my $path = Variant(VT_BSTR|VT_BYREF, '');
 my $setupInfo = $session->GetMiKTeXSetupInfo();
 print "MiKTeX: $setupInfo->{'version'}\n";
 if ($setupInfo->{'sharedSetup'}) {
