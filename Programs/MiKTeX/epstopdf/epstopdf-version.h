a94a09ccab3dddb51f4e1e9aa446f6e9a97351c5
csc <csc@mycompany.com>
2007-07-30 08:14:25 +0000

    Merged 2.5 changes r853:854 into the 2.6 branch.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.6@855 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/MiKTeX/epstopdf/epstopdf-version.h
+++ Programs/MiKTeX/epstopdf/epstopdf-version.h
@@ -1,6 +1,6 @@
 /* epstopdf-version.h: version number				-*- C++ -*-
 
-   Copyright (C) 2000-2006 Christian Schenk
+   Copyright (C) 2000-2007 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,6 +17,6 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMPONENT_VERSION 2329
+#define MIKTEX_COMPONENT_VERSION 2767
 
 #include <miktex/version.h>
