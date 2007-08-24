d93e30a76e18b306aa96aa034e6f97cb889ac63e
csc <csc@mycompany.com>
2007-08-24 14:45:17 +0000

    pdftex reads pdftex.map (was psfonts.map).
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@898 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/TeXAndFriends/pdftex/pdftex-version.h
+++ Programs/TeXAndFriends/pdftex/pdftex-version.h
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMPONENT_VERSION 2783
+#define MIKTEX_COMPONENT_VERSION 2792
 #define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "1.40.4"
 
 #include <miktex/version.h>
