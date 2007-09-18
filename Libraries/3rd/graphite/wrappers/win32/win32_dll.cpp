e8573add7a07589efb50d0a5d3d3e81bc55b56fa
csc <csc@mycompany.com>
2007-09-18 06:21:57 +0000

    Graphite engine 2.2.0.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@925 3fc76331-e2db-4b81-af9c-3600f57961af

---
+++ Libraries/3rd/graphite/wrappers/win32/win32_dll.cpp
@@ -0,0 +1,48 @@
+
+#include <windows.h>
+#include <crtdbg.h>
+#include <malloc.h>
+
+HMODULE s_hmod;
+
+HMODULE Win32DllGetModuleHandle()
+{
+	return s_hmod;
+}
+
+/*----------------------------------------------------------------------------------------------
+	DllMain. This is the main DLL entry point for a non-MFC DLL. For an MFC DLL, DllMain is
+		in DllModul.cpp. Both DllMains call ModuleEntry::DllMain.
+----------------------------------------------------------------------------------------------*/
+extern "C" BOOL WINAPI DllMain(HMODULE hmod, DWORD dwReason, PVOID pvReserved)
+{
+	bool fRet = true;
+
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		// REVIEW DarrellZ: This is temporary until MSFT fixes the realloc bug.
+		// See the comment at the beginning of ModuleEntry::WinMain.
+		_set_sbh_threshold(0);
+
+		s_hmod = hmod;
+		break;
+
+	case DLL_THREAD_ATTACH:
+		break;
+
+	case DLL_PROCESS_DETACH:
+		// Check for memory leaks.
+//		_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
+		break;
+
+	case DLL_THREAD_DETACH:
+		break;
+
+	default:
+		fRet = false;
+		break;
+	}
+
+	return fRet;
+}
