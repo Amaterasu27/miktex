741121c81537201ce513942203b0c9064fc58d84
csc <csc@mycompany.com>
2007-11-17 20:13:39 +0000

    Color support. Fixes bug #1829563.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1115 3fc76331-e2db-4b81-af9c-3600f57961af

---
+++ Programs/TeXAndFriends/mpost/mpware/dvitomp-miktex.h
@@ -0,0 +1,104 @@
+/* dvitomp-miktex.h:						-*- C++ -*-
+
+   Copyright (C) 1991-2007 Christian Schenk
+
+   This file is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 2, or (at your
+   option) any later version.
+
+   This file is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this file; if not, write to the Free Software
+   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA. */
+
+#if defined(_MSC_VER)
+#  pragma once
+#endif
+
+#define THEDATA(x) DVITOMPDATA.m_##x
+
+#include "dvitomp.rc"
+#include <MiKTeX/TeXAndFriends/WebApp>
+
+using namespace MiKTeX::Core;
+using namespace MiKTeX::TeXAndFriends;
+
+class DVITOMPCLASS
+
+  : public WebApp
+
+{
+public:
+  virtual
+  const char *
+  MIKTEXTHISCALL
+  GetUsage ()
+    const
+  {
+    return (MIKTEXTEXT("[OPTION...] DVIFILE MPXFILE"));
+  }
+
+public:
+  virtual
+  void
+  MIKTEXTHISCALL
+  ProcessCommandLineOptions ()
+  {
+    WebApp::ProcessCommandLineOptions ();
+    if (C4P::GetArgC() != 3)
+      {
+	BadUsage ();
+      }
+  }
+};
+
+extern DVITOMPCLASS DVITOMPAPP;
+#define THEAPP DVITOMPAPP
+
+#include <MiKTeX/TeXAndFriends/WebApp.inliners>
+
+#include <MiKTeX/KPSE/Emulation>
+
+/* _________________________________________________________________________
+
+   xmallocchararray
+   _________________________________________________________________________ */
+
+inline
+char *
+xmallocchararray (/*[in]*/ size_t size)
+{
+  return (reinterpret_cast<char*>(xmalloc(size + 1)));
+}
+
+/* _________________________________________________________________________
+
+   stringcast
+   _________________________________________________________________________ */
+
+template<typename TYPE>
+inline
+char *
+stringcast (/*[in]*/ TYPE * p)
+{
+  return (reinterpret_cast<char*>(p));
+}
+
+/* _________________________________________________________________________
+
+   libcfree
+   _________________________________________________________________________ */
+
+inline
+void
+libcfree (/*[in]*/ void * ptr)
+{
+  free (ptr);
+}
+
