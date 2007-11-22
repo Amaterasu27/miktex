d7860eaa569f85366458e9d173c7c761a81a95c0
csc <csc@mycompany.com>
2007-11-22 15:50:59 +0000

    Preparing MiKTeX 2.7 (RTM).
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1123 3fc76331-e2db-4b81-af9c-3600f57961af

--- miktex-version.cmake
+++ miktex-version.cmake
@@ -23,12 +23,12 @@ set(MIKTEX_MAJOR_VERSION	2)
 set(MIKTEX_MINOR_VERSION	7)
 
 ## days since January 1, 2000
-set(MIKTEX_J2000_VERSION	2878)
+set(MIKTEX_J2000_VERSION	2881)
 
 set(MIKTEX_YEAR_VERSION         2007)
 
 ## the release number; only meaningful for rc/beta builds
-set(MIKTEX_RELEASE_NUM		5)
+set(MIKTEX_RELEASE_NUM		1)
 
 ## the release state; one of:
 ##   0 (general availability release)
@@ -36,7 +36,7 @@ set(MIKTEX_RELEASE_NUM		5)
 ##   2 (beta release)
 ##   3 (snapshot release)
 ##   4 (debug)
-set(MIKTEX_RELEASE_STATE 2)
+set(MIKTEX_RELEASE_STATE 1)
 
 ## sanity check
 if(MIKTEX_RELEASE_STATE EQUAL 0)
