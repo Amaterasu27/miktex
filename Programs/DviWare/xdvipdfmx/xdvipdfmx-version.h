48c1449f2ef80edcbc6020f9f2c1ffe972ed92eb
csc <csc@mycompany.com>
2007-12-22 21:19:55 +0000

    xdvipdfmx 0.5 svn 103.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.7@1144 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/DviWare/xdvipdfmx/xdvipdfmx-version.h
+++ Programs/DviWare/xdvipdfmx/xdvipdfmx-version.h
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMPONENT_VERSION 2882
-#define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "0.5 svn 102"
+#define MIKTEX_COMPONENT_VERSION 2912
+#define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "0.5 svn 103"
 
 #include <MiKTeX/Core/Version>
