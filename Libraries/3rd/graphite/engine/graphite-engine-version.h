9988b3c6225ce7ccd93f11a98329be180c4785b8
csc <csc@mycompany.com>
2008-02-15 19:55:46 +0000

    XeTeX xetex 0.997 svn 574.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.7@1244 3fc76331-e2db-4b81-af9c-3600f57961af

--- Libraries/3rd/graphite/engine/graphite-engine-version.h
+++ Libraries/3rd/graphite/engine/graphite-engine-version.h
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMPONENT_VERSION 2965
-#define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "2.2.1 svn 833"
+#define MIKTEX_COMPONENT_VERSION 2967
+#define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "2.2.1 svn 842"
 
 #include <miktex/Core/Version>
