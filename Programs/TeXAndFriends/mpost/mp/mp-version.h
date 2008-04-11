d968723587fd065b95d2038a7fd550d1907706d8
csc <csc@mycompany.com>
2008-04-11 17:23:59 +0000

    MetaPost 1.003.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.7@1311 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/TeXAndFriends/mpost/mp/mp-version.h
+++ Programs/TeXAndFriends/mpost/mp/mp-version.h
@@ -1,6 +1,6 @@
 /* mp-version.h: version number					-*- C++ -*-
 
-   Copyright (C) 1996-2007 Christian Schenk
+   Copyright (C) 1996-2008 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMPONENT_VERSION 2876
-#define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "1.001"
+#define MIKTEX_COMPONENT_VERSION 3023
+#define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "1.003"
 
 #include <miktex/Core/Version>
