d7bd9f88c7070c0b986e31b27714131e0c0f1f46
csc <csc@mycompany.com>
2008-05-28 18:43:06 +0000

    XeTeX 0.999.0 svn 705.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.7@1346 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/TeXAndFriends/xetex/xetex-version.h
+++ Programs/TeXAndFriends/xetex/xetex-version.h
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMPONENT_VERSION 3069
-#define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "0.990.0-dev svn 700"
+#define MIKTEX_COMPONENT_VERSION 3070
+#define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "0.990.0 svn 705"
 
 #include <miktex/Core/Version>
