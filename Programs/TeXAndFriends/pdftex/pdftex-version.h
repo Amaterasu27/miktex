6d4efe6b9332575ac431c3b903950c35f6eaa859
csc <csc@mycompany.com>
2008-11-19 14:30:40 +0000

    Increased trie_size.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.7@1513 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/TeXAndFriends/pdftex/pdftex-version.h
+++ Programs/TeXAndFriends/pdftex/pdftex-version.h
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMPONENT_VERSION 3147
+#define MIKTEX_COMPONENT_VERSION 3235
 #define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "1.40.9"
 
 #include <miktex/Core/Version>
