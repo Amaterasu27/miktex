cc3be52d22d062c04295483d0fbeca927db0d444
csc <csc@mycompany.com>
2008-11-19 14:16:59 +0000

    Upgraded to new tex4ht version.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.6@1508 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/Converters/tex4ht/htcmd-version.h
+++ Programs/Converters/tex4ht/htcmd-version.h
@@ -1,6 +1,6 @@
 /* htcmd-version.h: version number				-*- C++ -*-
 
-   Copyright (C) 1996-2006 Christian Schenk
+   Copyright (C) 1996-2008 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMPONENT_VERSION 2330
+#define MIKTEX_COMPONENT_VERSION 3217
 #define MIKTEX_COMPONENT_ORIGINAL_VERSION_STR "2004-12-16-16:20"
 
 #include <miktex/version.h>
