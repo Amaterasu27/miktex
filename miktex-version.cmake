610165d928888131983c692417daae66ca5cbc11
csc <csc@mycompany.com>
2009-06-30 18:32:08 +0000

    MiKTeX 2.8 Beta 4.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1666 3fc76331-e2db-4b81-af9c-3600f57961af

--- miktex-version.cmake
+++ miktex-version.cmake
@@ -23,7 +23,7 @@ set(MIKTEX_MAJOR_VERSION	2)
 set(MIKTEX_MINOR_VERSION	8)
 
 ## days since January 1, 2000
-set(MIKTEX_J2000_VERSION	3459)
+set(MIKTEX_J2000_VERSION	3468)
 
 ## the version of the binary package
 set(MIKTEX_PACKAGE_REVISION	1)
