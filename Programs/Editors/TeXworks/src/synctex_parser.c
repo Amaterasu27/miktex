d2c838a0734cad9f8397e17b02ff06b0bd5065b4
csc <csc@mycompany.com>
2009-07-31 07:49:36 +0000

    TeXworks svn 346.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1713 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/Editors/TeXworks/src/synctex_parser.c
+++ Programs/Editors/TeXworks/src/synctex_parser.c
@@ -76,12 +76,7 @@ void *_synctex_malloc(size_t size);
 void *_synctex_malloc(size_t size) {
 	void * ptr = malloc(size);
 	if(ptr) {
-/*  In Visual C, bzero is not available */
-#ifdef _MSC_VER
 		memset(ptr,0, size);
-#else
-		bzero(ptr,size);
-#endif
 	}
 	return (void *)ptr;
 }
