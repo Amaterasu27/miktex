b52469486ed0cc639b99997cca2baf1bf07be0c9
csc <csc@mycompany.com>
2010-06-10 10:41:39 +0000

    libpng 1.4.2.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1843 3fc76331-e2db-4b81-af9c-3600f57961af

--- Libraries/3rd/libpng/libpng-version.h
+++ Libraries/3rd/libpng/libpng-version.h
@@ -1,6 +1,6 @@
 /* libpng-version.h: version number				-*- C++ -*-
 
-   Copyright (C) 1996-2008 Christian Schenk
+   Copyright (C) 1996-2010 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,10 +17,10 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 3275
-#define MIKTEX_COMP_ORIG_VERSION_STR "1.2.16"
+#define MIKTEX_COMP_J2000_VERSION 3813
+#define MIKTEX_COMP_ORIG_VERSION_STR "1.4.2"
 
-#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.; Copyright (c) 1996, 1997 Andreas Dilger; Copyright (c) 1998-2007 Glenn Randers-Pehrson"
-#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1995, 1996 Guy Eric Schalnat, Group 42, Inc.; Copyright (c) 1996, 1997 Andreas Dilger; @ 1998-2007 Glenn Randers-Pehrson"
+#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (c) 1998-2010 Glenn Randers-Pehrson"
+#define MIKTEX_COMP_COPYRIGHT_STR_1252 "Copyright © 1998-2010 Glenn Randers-Pehrson"
 
 #include <miktex/Core/Version>
