48aa91c1ee47dce7e24bc974aeaed5d7387872c8
csc <csc@mycompany.com>
2010-06-17 18:05:38 +0000

    BiBTeX 0.99d.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1864 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/Bibliography/bibtex/bibtex-version.h
+++ Programs/Bibliography/bibtex/bibtex-version.h
@@ -1,6 +1,6 @@
 /* bibtex-version.h: version number				-*- C++ -*-
 
-   Copyright (C) 1996-2009 Christian Schenk
+   Copyright (C) 1996-2010 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,10 +17,10 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 3518
-#define MIKTEX_COMP_ORIG_VERSION_STR "0.99c"
+#define MIKTEX_COMP_J2000_VERSION 3819
+#define MIKTEX_COMP_ORIG_VERSION_STR "0.99d"
 
-#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1985 by Oren Patashnik; all rights reserved."
-#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1985 by Oren Patashnik; all rights reserved."
+#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1985, 1988, 2010 Oren Patashnik; all rights reserved."
+#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1985, 1988, 2010 Oren Patashnik; all rights reserved."
 
 #include <miktex/Core/Version>
