b4edd080444092a2ed4ba1ec940c3f3f5c7545fb
csc <csc@mycompany.com>
2010-06-18 14:52:45 +0000

    pdftex 1.40.11 TL 2010.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1871 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/TeXAndFriends/pdftex/pdftex-version.h
+++ Programs/TeXAndFriends/pdftex/pdftex-version.h
@@ -17,8 +17,8 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 3816
-#define MIKTEX_COMP_ORIG_VERSION_STR "1.40.11"
+#define MIKTEX_COMP_J2000_VERSION 3821
+#define MIKTEX_COMP_ORIG_VERSION_STR "1.40.11 TL 2010"
 
 #define MIKTEX_COMP_COPYRIGHT_STR "\
 Copyright (C) 1982 D. E. Knuth, \
