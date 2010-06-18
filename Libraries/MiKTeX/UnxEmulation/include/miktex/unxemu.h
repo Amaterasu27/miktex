2f5e6dfc74059fdb5e343361c0636e32d80931e9
csc <csc@mycompany.com>
2010-06-18 15:16:55 +0000

    Unx emulation.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1878 3fc76331-e2db-4b81-af9c-3600f57961af

--- Libraries/MiKTeX/UnxEmulation/include/miktex/unxemu.h
+++ Libraries/MiKTeX/UnxEmulation/include/miktex/unxemu.h
@@ -1,6 +1,6 @@
 /* miktex/unxemu.h: Unx emulation				-*- C++ -*-
 
-   Copyright (C) 2007-2008 Christian Schenk
+   Copyright (C) 2007-2010 Christian Schenk
 
    This file is part of the MiKTeX UNXEMU Library.
 
@@ -24,7 +24,10 @@
 #endif
 
 #include <miktex/Core/Definitions>
+#include <miktex/Core/Core.h>
+#include <string.h>
 #include <sys/stat.h>
+#include <malloc.h>
 
 // DLL import/export switch
 #if ! defined(D2A2BA842ACE40C6A8A17A9358F2147E)
@@ -38,13 +41,63 @@
 // API decoration for exported functions
 #define MIKTEXUNXCEEAPI(type) MIKTEXUNXEXPORT type MIKTEXCEECALL
 
+/* _________________________________________________________________________
 
-#define S_ISDIR(m) (((m) & _S_IFDIR) != 0)
+   stdio.h
+   _________________________________________________________________________ */
+
+#define pclose _pclose
+#define popen _popen
+#define snprintf _snprintf
+
+/* _________________________________________________________________________
+
+   ctype.h
+   _________________________________________________________________________ */
+
+#define isblank(c) ((c) == ' ' || (c) == '\t')
+
+/* _________________________________________________________________________
+
+   strings.h
+   _________________________________________________________________________ */
+
+#define bzero(ptr, len) memset(ptr, 0, len)
+
+#if defined(__cplusplus)
+#  define strcasecmp(s1, s2) MiKTeX::Core::StringCompare(s1, s2, true)
+#else
+#  define strcasecmp(s1, s2) miktex_strcasecmp(s1, s2)
+#endif
+
+/* _________________________________________________________________________
+
+   unistd.h
+   _________________________________________________________________________ */
 
 #define F_OK 0
 #define W_OK 2
 #define R_OK 4
 #define X_OK W_OK
+
+/* _________________________________________________________________________
+
+   alloca.h
+   _________________________________________________________________________ */
+
+#define alloca _malloca
+
+/* _________________________________________________________________________
+
+   sys/stat.h
+   _________________________________________________________________________ */
+
+#define S_ISDIR(m) (((m) & _S_IFDIR) != 0)
+
+/* _________________________________________________________________________
+
+   dirent.h
+   _________________________________________________________________________ */
 
 #if defined(__cplusplus)
 extern "C" {
