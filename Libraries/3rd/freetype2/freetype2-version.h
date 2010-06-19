a2878c7762cc2321adf03d5b290c0dce77d43b12
csc <csc@mycompany.com>
2010-06-19 08:20:05 +0000

    freetype 2.3.11 TL 2010.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1891 3fc76331-e2db-4b81-af9c-3600f57961af

--- Libraries/3rd/freetype2/freetype2-version.h
+++ Libraries/3rd/freetype2/freetype2-version.h
@@ -17,8 +17,8 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 3275
-#define MIKTEX_COMP_ORIG_VERSION_STR "2.3.7"
+#define MIKTEX_COMP_J2000_VERSION 3822
+#define MIKTEX_COMP_ORIG_VERSION_STR "2.3.11"
 
 #define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1996-2001, 2002, 2003, 2004, 2006, 2007 by David Turner, Robert Wilhelm, and Werner Lemberg."
 #define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1996-2001, 2002, 2003, 2004, 2006, 2007 by David Turner, Robert Wilhelm, and Werner Lemberg."
