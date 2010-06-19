ef0acb5cd21a322b8f972a69613ff04aea48c7f0
csc <csc@mycompany.com>
2010-06-19 07:30:43 +0000

    dvips 5.99 TL 2010.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1890 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/DviWare/dvips/dvips-version.h
+++ Programs/DviWare/dvips/dvips-version.h
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 3819
+#define MIKTEX_COMP_J2000_VERSION 3822
 #define MIKTEX_COMP_ORIG_VERSION_STR "5.99 TL 2010"
 
 #define MIKTEX_COMP_COPYRIGHT_STR "(C) 1986-2010 Tomas Rokicki"
