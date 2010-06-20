ea8a382881a41bea7c9c8b24fc1e73d1fa426fd6
csc <csc@mycompany.com>
2010-06-20 17:50:26 +0000

    libbz2 1.0.5.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1892 3fc76331-e2db-4b81-af9c-3600f57961af

--- Libraries/3rd/bzip2/libbz2-version.h
+++ Libraries/3rd/bzip2/libbz2-version.h
@@ -1,6 +1,6 @@
 /* libbz2-version.h: version number				-*- C++ -*-
 
-   Copyright (C) 1996-2008 Christian Schenk
+   Copyright (C) 1996-2010 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,10 +17,10 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 3275
-#define MIKTEX_COMP_ORIG_VERSION_STR "1.0.4"
+#define MIKTEX_COMP_J2000_VERSION 3822
+#define MIKTEX_COMP_ORIG_VERSION_STR "1.0.5"
 
-#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1996-2006 Julian R Seward.  All rights reserved."
-#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1996-2006 Julian R Seward.  All rights reserved."
+#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1996-2007 Julian R Seward"
+#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1996-2007 Julian R Seward"
 
 #include <miktex/Core/Version>
