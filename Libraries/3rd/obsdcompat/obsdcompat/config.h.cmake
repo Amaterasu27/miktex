02cc49106004620147ab61899f48f0f84e6c6f8c
csc <csc@mycompany.com>
2010-06-30 12:41:22 +0000

    openbsd-compat TL 2010.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1932 3fc76331-e2db-4b81-af9c-3600f57961af

---
+++ Libraries/3rd/obsdcompat/obsdcompat/config.h.cmake
@@ -0,0 +1,247 @@
+/* obsdcompat/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if building universal (internal helper macro) */
+#cmakedefine AC_APPLE_UNIVERSAL_BUILD
+
+/* Define if your snprintf is busted */
+#cmakedefine BROKEN_SNPRINTF
+
+/* Define to 1 if you have the `asprintf' function. */
+#cmakedefine HAVE_ASPRINTF
+
+/* OpenBSD's gcc has bounded */
+#cmakedefine HAVE_ATTRIBUTE__BOUNDED__
+
+/* OpenBSD's gcc has sentinel */
+#cmakedefine HAVE_ATTRIBUTE__SENTINEL__
+
+/* Define to 1 if you have the `bcopy' function. */
+#cmakedefine HAVE_BCOPY
+
+/* Define if you are on Cygwin */
+#cmakedefine HAVE_CYGWIN
+
+/* Define to 1 if you have the <endian.h> header file. */
+#cmakedefine HAVE_ENDIAN_H
+
+/* Define to 1 if you have the <features.h> header file. */
+#cmakedefine HAVE_FEATURES_H
+
+/* Define to 1 if you have the <floatingpoint.h> header file. */
+#cmakedefine HAVE_FLOATINGPOINT_H
+
+/* Define to 1 if you have the `getrusage' function. */
+#cmakedefine HAVE_GETRUSAGE
+
+/* define if you have int64_t data type */
+#cmakedefine HAVE_INT64_T
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine HAVE_INTTYPES_H
+
+/* define if you have intxx_t data type */
+#cmakedefine HAVE_INTXX_T
+
+/* Define to 1 if you have the `dl' library (-ldl). */
+#cmakedefine HAVE_LIBDL
+
+/* Define to 1 if you have the <limits.h> header file. */
+#cmakedefine HAVE_LIMITS_H
+
+/* Define to 1 if the system has the type `long double'. */
+#cmakedefine HAVE_LONG_DOUBLE
+
+/* Define to 1 if the system has the type `long long'. */
+#cmakedefine HAVE_LONG_LONG
+
+/* Define to 1 if you have the <memory.h> header file. */
+#cmakedefine HAVE_MEMORY_H
+
+/* define if you have size_t data type */
+#cmakedefine HAVE_SIZE_T
+
+/* Define to 1 if you have the `snprintf' function. */
+#cmakedefine HAVE_SNPRINTF
+
+/* define if you have ssize_t data type */
+#cmakedefine HAVE_SSIZE_T
+
+/* Define to 1 if you have the <stddef.h> header file. */
+#cmakedefine HAVE_STDDEF_H
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDLIB_H
+
+/* Define to 1 if you have the `strdup' function. */
+#cmakedefine HAVE_STRDUP
+
+/* Define to 1 if you have the `strerror' function. */
+#cmakedefine HAVE_STRERROR
+
+/* Define to 1 if you have the `strftime' function. */
+#cmakedefine HAVE_STRFTIME
+
+/* Define to 1 if you have the <strings.h> header file. */
+#cmakedefine HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H
+
+/* Define to 1 if you have the `strlcat' function. */
+#cmakedefine HAVE_STRLCAT
+
+/* Define to 1 if you have the `strlcpy' function. */
+#cmakedefine HAVE_STRLCPY
+
+/* Define to 1 if you have the `strsep' function. */
+#cmakedefine HAVE_STRSEP
+
+/* Define to 1 if you have the `strtoll' function. */
+#cmakedefine HAVE_STRTOLL
+
+/* Define to 1 if you have the `strtonum' function. */
+#cmakedefine HAVE_STRTONUM
+
+/* Define to 1 if you have the `strtoul' function. */
+#cmakedefine HAVE_STRTOUL
+
+/* Define to 1 if you have the `sysconf' function. */
+#cmakedefine HAVE_SYSCONF
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/stropts.h> header file. */
+#cmakedefine HAVE_SYS_STROPTS_H
+
+/* Define to 1 if you have the <sys/strtio.h> header file. */
+#cmakedefine HAVE_SYS_STRTIO_H
+
+/* Define to 1 if you have the <sys/sysmacros.h> header file. */
+#cmakedefine HAVE_SYS_SYSMACROS_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine HAVE_SYS_TYPES_H
+
+/* define if you have uintxx_t data type */
+#cmakedefine HAVE_UINTXX_T
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H
+
+/* Define to 1 if the system has the type `unsigned long long'. */
+#cmakedefine HAVE_UNSIGNED_LONG_LONG
+
+/* Define to 1 if you have the <util.h> header file. */
+#cmakedefine HAVE_UTIL_H
+
+/* define if you have u_char data type */
+#cmakedefine HAVE_U_CHAR
+
+/* define if you have u_int data type */
+#cmakedefine HAVE_U_INT
+
+/* define if you have u_int64_t data type */
+#cmakedefine HAVE_U_INT64_T
+
+/* define if you have u_intxx_t data type */
+#cmakedefine HAVE_U_INTXX_T
+
+/* Define to 1 if you have the `vasprintf' function. */
+#cmakedefine HAVE_VASPRINTF
+
+/* Define if va_copy exists */
+#cmakedefine HAVE_VA_COPY
+
+/* Define to 1 if you have the `vsnprintf' function. */
+#cmakedefine HAVE_VSNPRINTF
+
+/* Define if compiler implements __FUNCTION__ */
+#cmakedefine HAVE___FUNCTION__
+
+/* Define if libc defines __progname */
+#cmakedefine HAVE___PROGNAME
+
+/* Define if __va_copy exists */
+#cmakedefine HAVE___VA_COPY
+
+/* Define if compiler implements __func__ */
+#cmakedefine HAVE___func__
+
+/* max value of long long calculated by configure */
+#cmakedefine LLONG_MAX
+
+/* min value of long long calculated by configure */
+#cmakedefine LLONG_MIN
+
+/* Name of package */
+#cmakedefine PACKAGE
+
+/* Define to the address where bug reports for this package should be sent. */
+#cmakedefine PACKAGE_BUGREPORT
+
+/* Define to the full name of this package. */
+#cmakedefine PACKAGE_NAME
+
+/* Define to the full name and version of this package. */
+#cmakedefine PACKAGE_STRING
+
+/* Define to the one symbol short name of this package. */
+#cmakedefine PACKAGE_TARNAME
+
+/* Define to the home page for this package. */
+#cmakedefine PACKAGE_URL
+
+/* Define to the version of this package. */
+#cmakedefine PACKAGE_VERSION
+
+/* The size of `char', as computed by sizeof. */
+#cmakedefine SIZEOF_CHAR
+
+/* The size of `int', as computed by sizeof. */
+#cmakedefine SIZEOF_INT
+
+/* The size of `long int', as computed by sizeof. */
+#cmakedefine SIZEOF_LONG_INT
+
+/* The size of `long long int', as computed by sizeof. */
+#cmakedefine SIZEOF_LONG_LONG_INT
+
+/* The size of `short int', as computed by sizeof. */
+#cmakedefine SIZEOF_SHORT_INT
+
+/* Define as const if snprintf() can declare const char *fmt */
+#cmakedefine SNPRINTF_CONST
+
+/* Define to 1 if you have the ANSI C header files. */
+#cmakedefine STDC_HEADERS
+
+/* Version number of package */
+#cmakedefine VERSION
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+#  undef WORDS_BIGENDIAN
+# endif
+#endif
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+#cmakedefine _FILE_OFFSET_BITS
+
+/* Define for large files, on AIX-style hosts. */
+#cmakedefine _LARGE_FILES
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+#cmakedefine inline
+#endif
