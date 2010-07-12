83d9564aaa9500f0ab3bebc86e7958445c6a2b47
csc <csc@mycompany.com>
2010-07-12 19:08:41 +0000

    0.9997.4 TL 2010.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1950 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/TeXAndFriends/xetex/xetex-version.h
+++ Programs/TeXAndFriends/xetex/xetex-version.h
@@ -17,8 +17,8 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 3832
-#define MIKTEX_COMP_ORIG_VERSION_STR "0.9997.3"
+#define MIKTEX_COMP_J2000_VERSION 3845
+#define MIKTEX_COMP_ORIG_VERSION_STR "0.9997.4"
 
 #define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1982 D. E. Knuth, (C) 1994-2006 by SIL International"
 #define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1982 D. E. Knuth, © 1994-2008 by SIL International"
