4ad4b6b223621dde06fc338f26b920cb76f6d29d
csc <csc@mycompany.com>
2010-08-15 18:42:11 +0000

    Towards beta 1.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1975 3fc76331-e2db-4b81-af9c-3600f57961af

--- miktex-version.cmake
+++ miktex-version.cmake
@@ -23,7 +23,7 @@ set(MIKTEX_MAJOR_VERSION	2)
 set(MIKTEX_MINOR_VERSION	9)
 
 ## days since January 1, 2000
-set(MIKTEX_J2000_VERSION	3849)
+set(MIKTEX_J2000_VERSION	3878)
 
 ## the version of the binary package
 set(MIKTEX_PACKAGE_REVISION	1)
@@ -40,7 +40,7 @@ set(MIKTEX_RELEASE_NUM		1)
 ##   3 (snapshot release)
 ##   4 (debug)
 set(MIKTEX_RELEASE_STATE
-  4
+  2
   CACHE STRING
   "The MiKTeX release state."
 )
