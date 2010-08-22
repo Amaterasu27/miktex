2ea9d2a090dcc5b082bea2668003a1339a37039c
csc <csc@mycompany.com>
2010-08-22 17:27:44 +0000

    MetaPost 1.211.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@1976 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/TeXAndFriends/mpost/mplib/mpost-version.h
+++ Programs/TeXAndFriends/mpost/mplib/mpost-version.h
@@ -17,7 +17,7 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 3829
+#define MIKTEX_COMP_J2000_VERSION 3881
 #define MIKTEX_COMP_ORIG_VERSION_STR "1.211"
 
 #define MIKTEX_COMP_COPYRIGHT_STR "(C) 2009 AT&T Bell Laboratories"
