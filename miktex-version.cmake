05f89101f4728d85a9f22f908ef7c6006b5691a9
csc <csc@mycompany.com>
2011-01-08 09:46:06 +0000

    It's 2011.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@2027 3fc76331-e2db-4b81-af9c-3600f57961af

--- miktex-version.cmake
+++ miktex-version.cmake
@@ -1,6 +1,6 @@
 ## miktex-version.cmake: define the MiKTeX version number
 ##
-## Copyright (C) 2006-2010 Christian Schenk
+## Copyright (C) 2006-2011 Christian Schenk
 ## 
 ## This file is free software; you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by
@@ -23,12 +23,12 @@ set(MIKTEX_MAJOR_VERSION	2)
 set(MIKTEX_MINOR_VERSION	9)
 
 ## days since January 1, 2000
-set(MIKTEX_J2000_VERSION	3972)
+set(MIKTEX_J2000_VERSION	4025)
 
 ## the version of the binary package
 set(MIKTEX_PACKAGE_REVISION	1)
 
-set(MIKTEX_YEAR_VERSION         2010)
+set(MIKTEX_YEAR_VERSION         2011)
 
 ## the release number; only meaningful for rc/beta builds
 set(MIKTEX_RELEASE_NUM		1)
