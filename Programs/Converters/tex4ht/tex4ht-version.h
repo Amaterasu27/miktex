dfd90cbd86d3a5fdb83e95c9d96ea0d1cded6a20
csc <csc@mycompany.com>
2011-07-12 19:26:49 +0000

    tex4ht 2010-12-16-08:39.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.9@2198 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/Converters/tex4ht/tex4ht-version.h
+++ Programs/Converters/tex4ht/tex4ht-version.h
@@ -17,10 +17,10 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 4196
-#define MIKTEX_COMP_ORIG_VERSION_STR "2009-01-31-07:33"
+#define MIKTEX_COMP_J2000_VERSION 4210
+#define MIKTEX_COMP_ORIG_VERSION_STR "2010-12-16-08:39"
 
-#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1996-2009 Eitan M. Gurari"
-#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1996-2009 Eitan M. Gurari"
+#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 2009-2010 TeX Users Group, Copyright (C) 1996-2009 Eitan M. Gurari"
+#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 2009-2010 TeX Users Group, © 1996-2009 Eitan M. Gurari"
 
 #include <miktex/Core/Version>
