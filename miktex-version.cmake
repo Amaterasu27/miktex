95a6ef6ebe39b23492b9c836a0fc743a0fdb7c32
csc <csc@mycompany.com>
2011-08-02 12:35:33 +0000

    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.9@2222 3fc76331-e2db-4b81-af9c-3600f57961af

--- miktex-version.cmake
+++ miktex-version.cmake
@@ -23,7 +23,7 @@ set(MIKTEX_MAJOR_VERSION	2)
 set(MIKTEX_MINOR_VERSION	9)
 
 ## days since January 1, 2000
-set(MIKTEX_J2000_VERSION	4227)
+set(MIKTEX_J2000_VERSION	4231)
 
 ## the version of the binary package
 set(MIKTEX_PACKAGE_REVISION	1)
