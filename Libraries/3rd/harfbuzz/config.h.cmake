34a0a73d12ae948658d56e32ced8a7323b05fe05
csc <csc@mycompany.com>
2013-03-27 16:02:16 +0000

    Added HarfBuzz library.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.9@2369 3fc76331-e2db-4b81-af9c-3600f57961af

---
+++ Libraries/3rd/harfbuzz/config.h.cmake
@@ -0,0 +1,147 @@
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `atexit' function. */
+#cmakedefine HAVE_ATEXIT 1
+
+/* Have cairo graphics library */
+#cmakedefine HAVE_CAIRO 1
+
+/* Have cairo-ft support in cairo graphics library */
+#cmakedefine HAVE_CAIRO_FT 1
+
+/* Have Core Text backend */
+#cmakedefine HAVE_CORETEXT 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#cmakedefine HAVE_DLFCN_H 1
+
+/* Have FreeType 2 library */
+#cmakedefine HAVE_FREETYPE 1
+
+/* Define to 1 if you have the `FT_Face_GetCharVariantIndex' function. */
+#cmakedefine HAVE_FT_FACE_GETCHARVARIANTINDEX 1
+
+/* Define to 1 if you have the `getpagesize' function. */
+#cmakedefine HAVE_GETPAGESIZE 1
+
+/* Have glib2 library */
+#cmakedefine HAVE_GLIB 1
+
+/* Have gobject2 library */
+#cmakedefine HAVE_GOBJECT 1
+
+/* Have Graphite library */
+#cmakedefine HAVE_GRAPHITE2 1
+
+/* Have gthread2 library */
+#cmakedefine HAVE_GTHREAD 1
+
+/* Have Old HarfBuzz backend */
+#cmakedefine HAVE_HB_OLD 1
+
+/* Have ICU library */
+#cmakedefine HAVE_ICU 1
+
+/* Have ICU Layout Engine library */
+#cmakedefine HAVE_ICU_LE 1
+
+/* Have Intel __sync_* atomic primitives */
+#cmakedefine HAVE_INTEL_ATOMIC_PRIMITIVES 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `isatty' function. */
+#cmakedefine HAVE_ISATTY 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#cmakedefine HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `mmap' function. */
+#cmakedefine HAVE_MMAP 1
+
+/* Define to 1 if you have the `mprotect' function. */
+#cmakedefine HAVE_MPROTECT 1
+
+/* Have native OpenType Layout backend */
+#cmakedefine HAVE_OT 1
+
+/* Have POSIX threads */
+#cmakedefine HAVE_PTHREAD 1
+
+/* Have PTHREAD_PRIO_INHERIT. */
+#cmakedefine HAVE_PTHREAD_PRIO_INHERIT 1
+
+/* Define to 1 if you have the <sched.h> header file. */
+#cmakedefine HAVE_SCHED_H 1
+
+/* Have sched_yield */
+#cmakedefine HAVE_SCHED_YIELD 1
+
+/* Have Solaris __machine_*_barrier and atomic_* operations */
+#cmakedefine HAVE_SOLARIS_ATOMIC_OPS 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#cmakedefine HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H 1
+
+/* Define to 1 if you have the `sysconf' function. */
+#cmakedefine HAVE_SYSCONF 1
+
+/* Define to 1 if you have the <sys/mman.h> header file. */
+#cmakedefine HAVE_SYS_MMAN_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine HAVE_SYS_TYPES_H 1
+
+/* Have UCDN Unicode functions */
+#cmakedefine HAVE_UCDN 1
+
+/* Have Uniscribe backend */
+#cmakedefine HAVE_UNISCRIBE 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#cmakedefine LT_OBJDIR 1
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+#cmakedefine NO_MINUS_C_MINUS_O 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#cmakedefine PACKAGE_BUGREPORT 1
+
+/* Define to the full name of this package. */
+#cmakedefine PACKAGE_NAME 1
+
+/* Define to the full name and version of this package. */
+#cmakedefine PACKAGE_STRING 1
+
+/* Define to the one symbol short name of this package. */
+#cmakedefine PACKAGE_TARNAME 1
+
+/* Define to the home page for this package. */
+#cmakedefine PACKAGE_URL 1
+
+/* Define to the version of this package. */
+#cmakedefine PACKAGE_VERSION 1
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+#cmakedefine PTHREAD_CREATE_JOINABLE 1
+
+/* Define to 1 if you have the ANSI C header files. */
+#cmakedefine STDC_HEADERS 1
