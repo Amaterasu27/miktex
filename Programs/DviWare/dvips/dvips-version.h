55ec7a3fc04af1abb8e7130c4b2024238132cd1c
csc <csc@mycompany.com>
2013-05-31 10:08:49 +0000

    dvips 5.993.
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.9@2424 3fc76331-e2db-4b81-af9c-3600f57961af

--- Programs/DviWare/dvips/dvips-version.h
+++ Programs/DviWare/dvips/dvips-version.h
@@ -17,10 +17,10 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 4784
-#define MIKTEX_COMP_ORIG_VERSION_STR "5.992"
+#define MIKTEX_COMP_J2000_VERSION 4899
+#define MIKTEX_COMP_ORIG_VERSION_STR "5.993"
 
-#define MIKTEX_COMP_COPYRIGHT_STR "(C) 1986-2012 Radical Eye Software"
-#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1986-2012 Radical Eye Software"
+#define MIKTEX_COMP_COPYRIGHT_STR "(C) 1986-2013 Radical Eye Software"
+#define MIKTEX_COMP_COPYRIGHT_STR_1252 "© 1986-2013 Radical Eye Software"
 
 #include <miktex/Core/Version>
