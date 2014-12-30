b11e71152ac4602d607a49c93e50763ac1231fe6
csc <csc@mycompany.com>
2014-12-30 16:24:35 +0000

    lua 5.2.3
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/branches/2.9@2490 3fc76331-e2db-4b81-af9c-3600f57961af

--- Libraries/3rd/lua/luac-version.h
+++ Libraries/3rd/lua/luac-version.h
@@ -1,6 +1,6 @@
 /* luac-version.h: version number				-*- C++ -*-
 
-   Copyright (C) 2010-2013 Christian Schenk
+   Copyright (C) 2010-2014 Christian Schenk
 
    This file is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published
@@ -17,10 +17,10 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 4897
-#define MIKTEX_COMP_ORIG_VERSION_STR "5.2.2"
+#define MIKTEX_COMP_J2000_VERSION 5477
+#define MIKTEX_COMP_ORIG_VERSION_STR "5.2.3"
 
-#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1994-2008 Lua.org, PUC-Rio.  All rights reserved."
-#define MIKTEX_COMP_COPYRIGHT_STR_1252 "Copyright © 1994-2008 Lua.org, PUC-Rio.  All rights reserved."
+#define MIKTEX_COMP_COPYRIGHT_STR "Copyright (C) 1994-2013 Lua.org, PUC-Rio"
+#define MIKTEX_COMP_COPYRIGHT_STR_1252 "Copyright © 1994-2013 Lua.org, PUC-Rio"
 
 #include <miktex/Core/Version>
