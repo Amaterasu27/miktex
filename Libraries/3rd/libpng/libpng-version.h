a0c31933c3704b6831fd65f5d5acecefe5680521
csc <csc@mycompany.com>
2015-04-11 13:57:03 +0000

    libpng 1.6.16
    git-svn-id: https://svn.code.sf.net/p/miktex/code/miktex/trunk@2582 3fc76331-e2db-4b81-af9c-3600f57961af

--- Libraries/3rd/libpng/libpng-version.h
+++ Libraries/3rd/libpng/libpng-version.h
@@ -17,8 +17,8 @@
    Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
-#define MIKTEX_COMP_J2000_VERSION 5570
-#define MIKTEX_COMP_ORIG_VERSION_STR "1.6.2"
+#define MIKTEX_COMP_J2000_VERSION 5576
+#define MIKTEX_COMP_ORIG_VERSION_STR "1.6.16"
 
 #define MIKTEX_COMP_COPYRIGHT_STR "Copyright (c) 1998-2012 Glenn Randers-Pehrson"
 #define MIKTEX_COMP_COPYRIGHT_STR_1252 "Copyright © 1998-2012 Glenn Randers-Pehrson"
